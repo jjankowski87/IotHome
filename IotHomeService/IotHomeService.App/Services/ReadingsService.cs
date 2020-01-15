344aa5b1119cb38f29f5b73b6d1dc9c127aa6827
Jakub Jankowski <jjankowski@future-processing.com>
2020-01-15 14:46:35 +0100

    Normalizing input date times to 15 min window, changed displayed time zone to Central European.

--- IotHomeService/IotHomeService.App/Services/ReadingsService.cs
+++ IotHomeService/IotHomeService.App/Services/ReadingsService.cs
@@ -10,6 +10,9 @@ namespace IotHomeService.App.Services
 {
     public class ReadingsService : IReadingsService
     {
+        private const int SamplingWindow = 15;
+        private const int HalfWindow = 7;
+
         private readonly IStorageExplorer _storageExplorer;
 
         public ReadingsService(IStorageExplorer storageExplorer)
@@ -19,31 +22,38 @@ namespace IotHomeService.App.Services
 
         public async Task<IDictionary<SensorDetails, IList<SensorReading>>> GetReadingsAsync(DateTime fromDate, DateTime toDate)
         {
-            var readings = await _storageExplorer.ListSensorDetailsAsync(fromDate, toDate);
+            // TODO: validate date range
+            var from = NormalizeDateTime(fromDate);
+            var to = NormalizeDateTime(toDate);
+
+            var readings = await _storageExplorer.ListSensorDetailsAsync(from, to);
 
             return readings.GroupBy(r => new SensorDetails(r.Body.Sensor, r.Body.Name))
-                .ToDictionary(g => g.Key, g => (IList<SensorReading>)AdjustReadings(g.ToList(), fromDate, toDate).ToList());
+                .ToDictionary(g => g.Key, g => (IList<SensorReading>)AdjustReadings(g.ToList(), from, to).ToList());
         }
 
-        private static IEnumerable<SensorReading> AdjustReadings(IList<IotMessage<Reading>> readings, DateTime fromDate, DateTime toDate)
+        private static IEnumerable<SensorReading> AdjustReadings(IList<IotMessage<Reading>> readings, DateTime from, DateTime to)
         {
-            const int samplingWindow = 15;
-            const int halfWindow = 7;
-
-            var from = fromDate.ToUniversalTime();
-            var to = toDate.ToUniversalTime();
             var window = (to - from).TotalMinutes;
 
             var result = new List<SensorReading>();
-            foreach (var readingTime in Enumerable.Range(0, (int)Math.Ceiling(window / samplingWindow) + 1).Select(i => from.AddMinutes(i * samplingWindow)))
+            foreach (var readingTime in Enumerable.Range(0, (int)Math.Ceiling(window / SamplingWindow) + 1).Select(i => from.AddMinutes(i * SamplingWindow)))
             {
-                var matching = readings.Where(r => Math.Abs((r.EnqueuedTimeUtc - readingTime).TotalMinutes) <= halfWindow).ToList();
+                var matching = readings.Where(r => Math.Abs((r.EnqueuedTimeUtc - readingTime).TotalMinutes) <= HalfWindow).ToList();
 
                 result.Add(new SensorReading(new DateTimeOffset(readingTime),
                     matching.Any() ? matching.Average(m => m.Body.Value) : (decimal?) null));
             }
 
             return result;
+        }
+
+        private static DateTime NormalizeDateTime(DateTime dateTime)
+        {
+            var universalDateTime = dateTime.ToUniversalTime();
+            var universalTime = universalDateTime.TimeOfDay;
+            
+            return universalDateTime.Date.AddMinutes(Math.Round(universalTime.TotalMinutes / SamplingWindow) * SamplingWindow);
         }
     }
 }
