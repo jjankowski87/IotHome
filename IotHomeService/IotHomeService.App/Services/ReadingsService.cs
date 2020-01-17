4eb084a94660e93429021444037dba89d8a9f3da
Jakub Jankowski <jjankowski@future-processing.com>
2020-01-17 12:22:18 +0100

    Sampling window depends on window size, max window size = 7 days.

--- IotHomeService/IotHomeService.App/Services/ReadingsService.cs
+++ IotHomeService/IotHomeService.App/Services/ReadingsService.cs
@@ -11,10 +11,14 @@ namespace IotHomeService.App.Services
 {
     public class ReadingsService : IReadingsService
     {
-        // TODO: sampling should depend on window size
-        // 15min, 30min, 1h etc
-        private const int SamplingWindow = 15;
-        private const int HalfWindow = 7;
+        private const int DefaultWindowMinutes = 60;
+        private static readonly IDictionary<TimeSpan, TimeSpan> SamplingWindows = new Dictionary<TimeSpan, TimeSpan>
+        {
+            { TimeSpan.FromHours(2), TimeSpan.FromMinutes(5) },
+            { TimeSpan.FromHours(4), TimeSpan.FromMinutes(10) },
+            { TimeSpan.FromHours(8), TimeSpan.FromMinutes(15) },
+            { TimeSpan.FromHours(24), TimeSpan.FromMinutes(30) }
+        };
 
         private readonly IStorageExplorer _storageExplorer;
         private readonly AppConfiguration _configuration;
@@ -27,42 +31,59 @@ namespace IotHomeService.App.Services
 
         public async Task<IDictionary<SensorDetails, IList<SensorReading>>> GetReadingsAsync(DateTime fromDate, DateTime toDate)
         {
-            // TODO: validate date range
-            var from = NormalizeDateTime(fromDate);
-            var to = NormalizeDateTime(toDate);
+            if (toDate < fromDate || (toDate - fromDate) > TimeSpan.FromDays(7))
+            {
+                return new Dictionary<SensorDetails, IList<SensorReading>>();
+            }
+
+            var samplingWindow = SelectSamplingWindow(fromDate, toDate);
+            var from = NormalizeDateTime(fromDate, samplingWindow);
+            var to = NormalizeDateTime(toDate, samplingWindow);
 
             var readings = await _storageExplorer.ListSensorDetailsAsync(@from, to);
 
             return readings.GroupBy(r => new SensorDetails(r.Body.Sensor, r.Body.Name))
-                .ToDictionary(g => g.Key, g => (IList<SensorReading>)AdjustReadings(g.ToList(), from, to).ToList());
+                .ToDictionary(g => g.Key, g => (IList<SensorReading>)AdjustReadings(g.ToList(), from, to, samplingWindow).ToList());
         }
 
-        private IEnumerable<SensorReading> AdjustReadings(IList<IotMessage<Reading>> readings, DateTimeOffset from, DateTimeOffset to)
+        private IEnumerable<SensorReading> AdjustReadings(IList<IotMessage<Reading>> readings, DateTimeOffset from, DateTimeOffset to, int samplingWindow)
         {
             var window = (to - from).TotalMinutes;
 
             var result = new List<SensorReading>();
-            foreach (var readingTime in Enumerable.Range(0, (int)Math.Ceiling(window / SamplingWindow) + 1).Select(i => from.AddMinutes(i * SamplingWindow)))
+            foreach (var readingTime in Enumerable.Range(0, (int)Math.Ceiling(window / samplingWindow) + 1).Select(i => from.AddMinutes(i * samplingWindow)))
             {
-                var matching = readings.Where(r => Math.Abs((r.EnqueuedTimeUtc - readingTime).TotalMinutes) <= HalfWindow).ToList();
+                var matching = readings.Where(r => Math.Abs((r.EnqueuedTimeUtc - readingTime).TotalMinutes) <= (samplingWindow / 2)).ToList();
                 var localTime = TimeZoneInfo.ConvertTime(readingTime, _configuration.ApplicationTimeZone);
 
                 result.Add(new SensorReading(localTime,
-                    matching.Any() ? matching.Average(m => m.Body.Value) : (decimal?) null));
+                    matching.Any() ? Math.Round(matching.Average(m => m.Body.Value), 2) : (decimal?) null));
             }
 
             return result;
         }
 
-        private DateTimeOffset NormalizeDateTime(DateTime dateTime)
+        private DateTimeOffset NormalizeDateTime(DateTime dateTime, int samplingWindow)
         {
             var utc = dateTime.Kind == DateTimeKind.Utc
                 ? new DateTimeOffset(dateTime, TimeSpan.Zero)
                 : new DateTimeOffset(dateTime, _configuration.ApplicationTimeZone.GetUtcOffset(dateTime)).ToUniversalTime();
 
-            var normalizedMinutes = Math.Round(utc.TimeOfDay.TotalMinutes / SamplingWindow) * SamplingWindow;
+            var normalizedMinutes = Math.Round(utc.TimeOfDay.TotalMinutes / samplingWindow) * samplingWindow;
 
             return utc.Add(-utc.TimeOfDay).AddMinutes(normalizedMinutes);
+        }
+
+        private static int SelectSamplingWindow(DateTime fromDate, DateTime toDate)
+        {
+            var window = (toDate - fromDate).TotalMinutes;
+            var matchingWindow = SamplingWindows.FirstOrDefault(sw => window <= sw.Key.TotalMinutes);
+            if (matchingWindow.Equals(default(KeyValuePair<TimeSpan, TimeSpan>)))
+            {
+                return DefaultWindowMinutes;
+            }
+
+            return (int)matchingWindow.Value.TotalMinutes;
         }
     }
 }
